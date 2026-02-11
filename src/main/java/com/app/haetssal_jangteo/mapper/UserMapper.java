13de8b90b1ffe12bc96a804b817376315dad1bd4
kmj <sokkomann@gmail.com>
2026-02-11 17:01:13 +0900

    회원가입 작성중
    # Conflicts:
    #	src/main/java/com/app/haetssal_jangteo/audit/Period.java
    #	src/main/resources/application.yaml

---
+++ src/main/java/com/app/haetssal_jangteo/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.app.haetssal_jangteo.mapper;
+
+import com.app.haetssal_jangteo.common.enumeration.User;
+import com.app.haetssal_jangteo.domain.OAuthVO;
+import com.app.haetssal_jangteo.domain.UserVO;
+import com.app.haetssal_jangteo.dto.UserDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
+
+@Mapper
+public interface UserMapper {
+
+// 회원가입
+    public void insert(UserDTO userDTO);
+
+    public void insertOauth(OAuthVO oAuthVO);
+
+    public Optional<UserDTO> selectByEmail(String userEmail);
+
+    public Optional<UserVO> selectUserForLogin(UserDTO userDTO);
+}
